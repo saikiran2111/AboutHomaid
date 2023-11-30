d387624f64664cca46908112b58746ae1a09af0f
S Sai Kiran <133565317+saikiran2111@users.noreply.github.com>
2023-11-30 17:02:22 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselInner = document.querySelector(".carousel-inner");
+  
+    let currentIndex = 0;
+    const totalImages = document.querySelectorAll(".carousel-item").length;
+  
+    function showNext() {
+      currentIndex = (currentIndex + 1) % totalImages;
+      updateCarousel();
+    }
+  
+    function updateCarousel() {
+      const translateValue = -currentIndex * 100 + "%";
+      carouselInner.style.transform = "translateX(" + translateValue + ")";
+    }
+  
+    setInterval(showNext, 3000);
+  });
